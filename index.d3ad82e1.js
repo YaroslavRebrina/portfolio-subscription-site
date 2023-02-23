fa5200ddcbfd832e408822ea6231e775bedf6d52
YaroslavRebrina <83611757+YaroslavRebrina@users.noreply.github.com>
2023-02-23 17:06:06 +0000

    Deploying to gh-pages from @ YaroslavRebrina/portfolio-subscription-site@c9cba8f54544fe154be24d376515df5bd590426f 🚀

---
+++ index.d3ad82e1.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},autoplay:{delay:5e3}});
+//# sourceMappingURL=index.d3ad82e1.js.map
--- index.f1cea686.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},autoplay:{delay:5e3}});
-//# sourceMappingURL=index.f1cea686.js.map
