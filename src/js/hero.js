c9cba8f54544fe154be24d376515df5bd590426f
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2023-02-23 19:05:34 +0200

    fixed slider

--- src/js/hero.js
+++ src/js/hero.js
@@ -5,11 +5,6 @@ const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
